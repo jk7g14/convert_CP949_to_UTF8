f4508d0bcb65d53333aec30306341c7fb3a14c8d
jk7g14 <jk7g14@gmail.com>
2020-01-22 18:00:33 +0900

    Update convert_CP949_to_UTF8.py

--- convert_CP949_to_UTF8.py
+++ convert_CP949_to_UTF8.py
@@ -4,6 +4,5 @@ path = 'web'
 filetypes = ['*.html','*.css','*.js']
 for filetype in filetypes:
     for filename in Path(path).rglob(filetype):
-        command = f'iconv -f CP949 -t UTF-8 -o {filename} {filename}'
-        print(command)
-        #os.system(command)
+        command = f'iconv -f CP949 -t UTF-8 -o {filename} {filename}'       
+        os.system(command)
